2f23df6bb29f66c87b269ed1555889510f265e74
Adan Jareth <yarethmanrique@gmail.com>
2021-01-07 13:01:40 -0600

    Clase 07/01/2021

--- ADT'S_Clase_EDD/Pruebas_colas.py
+++ ADT'S_Clase_EDD/Pruebas_colas.py
@@ -1,4 +1,4 @@
-from Colas import  Queue
+from Colas import  Queue, BoundedPriorityQueue
 
 q1 = Queue()
 q1.enqueue(3)
@@ -24,3 +24,21 @@ print(atencion.to_string())
 sig = atencion.dequeue()
 print(f"Bienvenido sr. {sig['nombre']}, en qué podemos servirle el día de hoy")
 print(atencion.to_string())
+print()
+print()
+print()
+print("PRUEBA DE LAS COLAS CON PRIORIDAD ACOTADA")
+print()
+maestres = {"Prioridad":4,"Descripcion":"Maestre","Personas":["Juan","Diego"]}
+niños = {"Prioridad":2,"Descripcion":"Niños","Personas":["Pedrito","Angel"]}
+mecanicos = {"Prioridad":4,"Descripcion":"Mecanicos","Personas":["José","Luis"]}
+mujeres = {"Prioridad":3,"Descripcion":"Mujeres","Personas":["Evelyn","Gaby"]}
+niñas = {"Prioridad":1,"Descripcion":"Niñas","Personas":["Ahri","Ana"]}
+
+cpa = BoundedPriorityQueue(7)
+cpa.enqueue(maestres['Prioridad'],maestres)
+cpa.enqueue(niños['Prioridad'],niños)
+cpa.enqueue(mecanicos['Prioridad'],mecanicos)
+cpa.enqueue(mujeres['Prioridad'],mujeres)
+cpa.enqueue(niñas['Prioridad'],niñas)
+cpa.to_string()
