f3a27362748472f2b22abc02e44fdd19671edc2d
Adan Jareth <yarethmanrique@gmail.com>
2021-01-19 13:08:51 -0600

    Clase 19/01/2021

---
+++ Arboles/prueba_ABB.py
@@ -0,0 +1,11 @@
+from arboles_binarios import BinarySearchTree
+
+abb = BinarySearchTree()
+abb.insert(50)
+abb.insert(30)
+abb.insert(40)
+abb.insert(60)
+abb.insert(70)
+abb.insert(80)
+
+abb.transversal()
