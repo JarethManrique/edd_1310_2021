5a01948cc39c2a5a05873d5f5abcf4b8004bd4e1
Adan Jareth <yarethmanrique@gmail.com>
2021-01-26 12:55:11 -0600

    Clase 20/01/2021

--- Arboles/arboles_binarios.py
+++ Arboles/arboles_binarios.py
@@ -83,12 +83,39 @@ class BinarySearchTree:
             return self.__search(nodo.right, value)
 
     def remove(self, value):
-        encontrado = self.search(value)
-        # CASO 1
-        if encontrado.left == None and encontrado.right == None:
-            print("Eliminando ", encontrado.data)
-            encontrado = None
-        # CASO 2
-        elif (encontrado.left != None and encontrado.right == None) or \
-             (encontrado.left == None and encontrado.right != None):
-             print("A eliminar: ", encontrado.data)
+        if self.__root == None:
+            print("Nodo que eliminar")
+        else:
+            self.__remove(None, None, self.__root, value)
+
+    def __remove(self, padre_hi, padre_hd, actual, value):
+        if actual == None:
+            print("Caso base")
+            return None
+        elif actual.data == value: # CASO BASE
+            print("Encontrado: ", actual.data)
+            # CASO 1 (Nodo Hoja)
+            if actual.left == None and actual.right == None:
+                if padre_hi != None:
+                    padre_hi.left = None
+                else:
+                    padre_hd.right = None
+            # Caso 2 (Con un hijo)
+            if (actual.left != None and actual.right == None) or (actual.left == None and actual.right != None):
+                print("Es un nodo con un hijo", actual.data)
+                if actual.left != None:
+                    actual.data = actual.left.data
+                    actual.left = None
+                else:
+                    actual.data = actual.right.data
+                    actual.right = None
+            # CASO 3 (Con 2 hijos)
+            
+            #return actual
+
+        elif value < actual.data:
+            print("Buscar a la izquierda")
+            self.__remove(actual, None, actual.left, value)
+        else:
+            print("Buscar a la derecha")
+            self.__remove(None, actual, actual.right, value)
